35dd50fac6e5af98d896ecdca97dc08a244aad5f
Rensy <rensy.aikara@gmail.com>
2020-05-11 22:21:04 -0500

    added basic routes

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,5 +1,19 @@
 var express = require("express");
+var path = require("path");
 
 var router = express.Router();
+
+router.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/about.html"));
+});
+
+router.get("/projects", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+});
+
 
 module.exports = router;
